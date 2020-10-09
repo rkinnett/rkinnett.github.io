45f8e6909e22ae39059485b237dac20461d601a9
rkinnett <rkinnett@gmail.com>
2020-10-08 23:19:00 -0700

    initial commit

---
+++ mars/js/mars.js
@@ -0,0 +1,78 @@
+// Created by Bjorn Sandvik - thematicmapping.org
+(function () {
+
+	var webglEl = document.getElementById('webgl');
+
+	if (!Detector.webgl) {
+		Detector.addGetWebGLMessage(webglEl);
+		return;
+	}
+
+	var width  = window.innerWidth,
+		height = window.innerHeight;
+
+	// Earth params
+	var radius   = 0.5,
+		segments = 32,
+		rotation = 6;  
+
+	var scene = new THREE.Scene();
+
+	var camera = new THREE.PerspectiveCamera(45, width / height, 0.01, 1000);
+	camera.position.z = 1.5;
+
+	var renderer = new THREE.WebGLRenderer();
+	renderer.setSize(width, height);
+
+	scene.add(new THREE.AmbientLight(0x333333));
+
+	var light = new THREE.DirectionalLight(0xffffff, 1);
+	light.position.set(5,3,5);
+	scene.add(light);
+
+    var sphere = createSphere(radius, segments);
+	sphere.rotation.y = rotation; 
+	scene.add(sphere)
+
+
+	var stars = createStars(90, 64);
+	scene.add(stars);
+
+	var controls = new THREE.TrackballControls(camera);
+
+	webglEl.appendChild(renderer.domElement);
+
+	render();
+
+	function render() {
+		controls.update();
+		sphere.rotation.y += 0.0005;
+		requestAnimationFrame(render);
+		renderer.render(scene, camera);
+	}
+
+	function createSphere(radius, segments) {
+		return new THREE.Mesh(
+			new THREE.SphereGeometry(radius, segments, segments),
+			new THREE.MeshPhongMaterial({
+				map:         THREE.ImageUtils.loadTexture('images/8k_mars.jpg'),
+				bumpMap:     THREE.ImageUtils.loadTexture('images/mars_bump_map_4k.png'),
+				bumpScale:   0.01,
+				//specularMap: THREE.ImageUtils.loadTexture('images/water_4k.png'),
+				//specular:    new THREE.Color('grey')								
+			})
+		);
+	}
+
+
+	function createStars(radius, segments) {
+		return new THREE.Mesh(
+			new THREE.SphereGeometry(radius, segments, segments), 
+			new THREE.MeshBasicMaterial({
+				map:  THREE.ImageUtils.loadTexture('images/galaxy_starfield.png'), 
+				side: THREE.BackSide
+			})
+		);
+	}
+
+}());
