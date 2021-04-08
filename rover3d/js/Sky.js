ca1e4df16d002aee1055696f5fb68bdc82bdd7ef
rkinnett <rkinnett@gmail.com>
2021-04-08 12:01:56 -0700

    cleanup sky shader

--- rover3d/js/Sky.js
+++ rover3d/js/Sky.js
@@ -70,66 +70,34 @@ Sky.SkyShader = {
 		'varying vec3 vBetaM;',
 		'varying float vSunE;',
 
-		// constants for atmospheric scattering
 		'const float e = 2.71828182845904523536028747135266249775724709369995957;',
 		'const float pi = 3.141592653589793238462643383279502884197169;',
+    'const float piOver2 = 1.57079632679;',
 
-		// wavelength of used primaries, according to preetham
-		//'const vec3 lambda = vec3( 680E-9, 550E-9, 450E-9 );',
-    // wavelength per Collienne
-    'const vec3 lambda = vec3( 680E-9, 510E-9, 440E-9 );',
-		// this pre-calcuation replaces older TotalRayleigh(vec3 lambda) function:
-		// (8.0 * pow(pi, 3.0) * pow(pow(n, 2.0) - 1.0, 2.0) * (6.0 + 3.0 * pn)) / (3.0 * N * pow(lambda, vec3(4.0)) * (6.0 - 7.0 * pn))
-		//'const vec3 totalRayleigh = vec3( 5.804542996261093E-6, 1.3562911419845635E-5, 3.0265902468824876E-5 );',
-    'const vec3 totalRayleigh = vec3( 3.4E-4, 2.0E-4, 0.9E-4 );',  // RK  - SKY COLOR
+    'const vec3 totalRayleigh = vec3( 3.4E-4, 2.0E-4, 0.9E-4 );',  // RK: Rayleight scatter color (main sky color)
 
-		// mie stuff
-		'const vec3 MieConst = vec3( 1.6E14, 4.6E14, 6.1E14 );',  // RK: affects horizon color at low elevation (higher is brighter)
-    //'const vec3 MieConst = vec3( 4.8399918514433978E14, 2.7798023919660528E14, 3.0790479543861094E14 );',
+		'const vec3 MieConst = vec3( 1.6, 3.6, 6.1 );',  // RK: Mie scattering "color"
 
-		// earth shadow hack
-		// cutoffAngle = pi / 1.95;
-		//'const float cutoffAngle = 1.6110731556870734;',
-    'const float cutoffAngle = 1.57;',  
-		//'const float steepness = 1.5;',
-    'const float steepness = 2.5;',
-		'const float EE = 1000.0;',
-
-		'float sunIntensity( float zenithAngleCos ) {',
-		'	zenithAngleCos = clamp( zenithAngleCos, -1.0, 1.0 );',
-		//'	return EE * max( 0.0, 1.0 - pow( e, -( ( cutoffAngle - acos( zenithAngleCos ) ) / steepness ) ) );',
-    '	return EE * max( 0.0, 1.0 - pow( e, -( ( cutoffAngle - acos( zenithAngleCos ) ) / steepness ) ) );',  //RK
-		'}',
-
-		'vec3 totalMie( float T ) {',
-		'	float c = ( 0.2 * T ) * 10E-18;',
-		'	return 0.434 * c * MieConst;',
-		'}',
+    'const float sunIntensityVsElevation = 1.0;', //(~1-10) (1.5 orig, 2.5 mars) RK: affects sky brightness vs sun elevation, higher=brighter
+		'const float sunIntensityZenith = 400.0;',  //(1.0-4000.0) RK: sky brightness scalar
+    
 
 		'void main() {',
 
 		'	vec4 worldPosition = modelMatrix * vec4( position, 1.0 );',
 		'	vWorldPosition = worldPosition.xyz;',
-
 		'	gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );',
-		'	gl_Position.z = gl_Position.w;', // set z to camera.far
-
+		'	gl_Position.z = gl_Position.w;',
 		'	vSunDirection = normalize( sunPosition );',
-
-		'	vSunE = sunIntensity( dot( vSunDirection, up ) );',
-
-		'	vSunfade = 1.0 - clamp( 1.0 - exp( ( -1.0*vSunDirection.z ) ), 0.0, 1.0 );',
-		//'	vSunfade = (1.0 - clamp( 1.0 - exp( ( 1*sunPosition.z / 100.0 ) ), 0.0, 1.0 )) *0.999 + 0.01;',
+		'	float zenithAngleCos = clamp( dot( vSunDirection, up ), -1.0, 1.0);',
+    '	vSunE = sunIntensityZenith * max( 0.0, 1.0 - pow( e, -( ( piOver2 - acos( zenithAngleCos ) ) / sunIntensityVsElevation ) ) );',
+		'	vSunfade = 1.0 - clamp( 1.0 - exp( ( -1.0 * vSunDirection.z ) ), 0.0, 1.0 );',
 
 		'	float rayleighCoefficient = rayleigh - ( 1.0 * ( 1.0 - vSunfade ) );',
-
-		// extinction (absorbtion + out scattering)
-		// rayleigh coefficients
 		'	vBetaR = totalRayleigh * rayleighCoefficient;',
 
-		// mie coefficients
-		'	vBetaM = totalMie( turbidity ) * mieCoefficient;',
-
+    ' vec3 totalMie = 0.434 * ( 0.2 * turbidity ) * 10E-4 * MieConst;',
+		'	vBetaM = totalMie * mieCoefficient;',
 		'}'
 	].join( '\n' ),
 
@@ -146,22 +114,19 @@ Sky.SkyShader = {
 
 		'const vec3 cameraPos = vec3( 0.0, 0.0, 0.0 );',
 
-		// constants for atmospheric scattering
 		'const float pi = 3.141592653589793238462643383279502884197169;',
-
-		'const float n = 1.0003;', // refractive index of air
-		//'const float N = 2.545E25;', // number of molecules per unit volume for air at 288.15K and 1013mb (sea level -45 celsius)
-    'const float N = 2.545E23;', // number of molecules per unit volume - density ~1% compared to Earth
-
-		// optical length at zenith for molecules
-		'const float rayleighZenithLength = 11.4E3;',
-		'const float mieZenithLength = 6.25E3;',
-    'const float sunAngularDiameterCos = 0.99997;',  //RK: slightly smaller sun  (was 0.9999566769)
-
-		// 3.0 / ( 16.0 * pi )
 		'const float THREE_OVER_SIXTEENPI = 0.05968310365946075;',
-		// 1.0 / ( 4.0 * pi )
 		'const float ONE_OVER_FOURPI = 0.07957747154594767;',
+    'const float RAD_TO_DEG = 57.2957795131;',
+
+
+		// optical length at zenith
+		'const float rayleighZenithLength   = 4.0E3;',   //RK: affects horizon appearance (10.0E3)
+		'const float mieZenithLength        = 1.0E3;',   //RK: affects horizon appearance (5.0E3)
+
+    // sun disk
+    'const float sunAngularDiameterCos  = 0.99998;',  //RK: higher value = smaller sun disk
+
 
 		'float rayleighPhase( float cosTheta ) {',
 		'	return THREE_OVER_SIXTEENPI * ( 1.0 + pow( cosTheta, 2.0 ) );',
@@ -178,16 +143,15 @@ Sky.SkyShader = {
 		'	vec3 direction = normalize( vWorldPosition - cameraPos );',
 
 		// optical length
-		// cutoff angle at 90 to avoid singularity in next formula.
 		'	float zenithAngle = acos( max( 0.0, dot( up, direction ) ) );',
-		'	float inverse = 1.0 / ( cos( zenithAngle ) + 0.15 * pow( 93.885 - ( ( zenithAngle * 180.0 ) / pi ), -1.253 ) );',
+		'	float inverse = 1.0 / ( cos( zenithAngle ) + 0.15 * pow( 93.885 - zenithAngle * RAD_TO_DEG, -1.253 ) );',
 		'	float sR = rayleighZenithLength * inverse;',
 		'	float sM = mieZenithLength * inverse;',
 
 		// combined extinction factor
 		'	vec3 Fex = exp( -( vBetaR * sR + vBetaM * sM ) );',
 
-		// in scattering
+		// in-scattering
 		'	float cosTheta = dot( direction, vSunDirection );',
 
 		'	float rPhase = rayleighPhase( cosTheta * 0.5 + 0.5 );',
@@ -197,18 +161,15 @@ Sky.SkyShader = {
 		'	vec3 betaMTheta = vBetaM * mPhase;',
 
 		'	vec3 Lin = pow( vSunE * ( ( betaRTheta + betaMTheta ) / ( vBetaR + vBetaM ) ) * ( 1.0 - Fex ), vec3( 1.5 ) );',
-		'	Lin *= mix( vec3( 1.0 ), pow( vSunE * ( ( betaRTheta + betaMTheta ) / ( vBetaR + vBetaM ) ) * Fex, vec3( 1.0 / 2.0 ) ), clamp( pow( 1.0 - dot( up, vSunDirection ), 5.0 ), 0.0, 1.0 ) );',
+		'	Lin *= mix( vec3( 1.0 ), pow( vSunE * ( ( betaRTheta + betaMTheta ) / ( vBetaR + vBetaM ) ) * Fex, vec3( 0.5 ) ), clamp( pow( 1.0 - dot( up, vSunDirection ), 5.0 ), 0.0, 1.0 ) );',
 
 		'	vec3 L0 = vec3( 0.1 ) * Fex;',
 
 		// composition + solar disc
 		'	float sundisk = smoothstep( sunAngularDiameterCos, sunAngularDiameterCos + 0.00002, cosTheta );',
 		'	L0 += ( vSunE * 19000.0 * Fex ) * sundisk;',
-
 		'	vec3 texColor = ( Lin + L0 ) * 0.04 + vec3( 0.0, 0.0003, 0.00075 );',
-
 		'	vec3 retColor = pow( texColor, vec3( 1.0 / ( 1.2 + ( 1.2 * vSunfade ) ) ) );',
-
 		' gl_FragColor = vec4( retColor, 1.0 );',
 
 		'#include <tonemapping_fragment>',
