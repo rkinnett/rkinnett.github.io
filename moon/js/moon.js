a09140845321cdca416b4f14375ecabf24c76b5a
rkinnett <rkinnett@gmail.com>
2026-06-27 13:50:03 -0700

    optimized dem

--- moon/js/moon.js
+++ moon/js/moon.js
@@ -967,7 +967,7 @@ function createGlobe(radius, segments) {
   globeLoaded = true;
 
   // Load elevation model:  
-  const bumpMapFile = 'data/lola_dem_8192.png';
+  const bumpMapFile = 'data/lola_dem_8192.jpg';
   console.log("loading bump map " + bumpMapFile);
   globe.material.bumpMap = new THREE.TextureLoader().load(bumpMapFile);
   globe.material.bumpMap.minFilter = THREE.LinearMipMapLinearFilter;
